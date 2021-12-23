88da97b03b120d9887d85f30a2703301ec4def3e
Anil Kumar <anilkumarreddy.a@techouts.com>
2021-12-23 11:47:28 +0530

    actions

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,4 +1,4 @@
-import { LOGIN_PASSWORD, LOGIN_USERNAME, STORE_TITLE  } from "./types"
+import { LOGIN_PASSWORD, LOGIN_USERNAME, STORE_TITLE ,ADD_ITEM_TO_CART, ITEM_COUNT, ITEM_COUNT_DECREASE, ITEM_COUNT_INCREASE, REMOVE_ITEM_FROM_CART } from "./types"
 
 export const loginUserName = (userName) => {
     return {
@@ -17,5 +17,39 @@ export const storeTitle = (title) => {
     return {
         type: STORE_TITLE,
         payload: title
+    }
+}
+
+export const itemCount = (count) => {
+    return {
+        type: ITEM_COUNT,
+        payload: count
+    }
+}
+
+export const itemCountIncrease = (countIncrease) => {
+    return {
+        type: ITEM_COUNT_INCREASE,
+        payload: countIncrease
+    }
+}
+
+export const itemCountDecrease = (countDecrease) => {
+    return {
+        type: ITEM_COUNT_DECREASE,
+        payload: countDecrease
+    }
+}
+
+export const addItemToCart = (items) =>{
+    return{
+        type:ADD_ITEM_TO_CART,
+        payload:items
+    }
+};
+
+export const removeItemFromCart = () =>{
+    return{
+        type:REMOVE_ITEM_FROM_CART
     }
 }
